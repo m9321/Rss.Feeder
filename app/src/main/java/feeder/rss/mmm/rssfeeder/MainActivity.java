db04cfc49c726a95043af20ccdc490d707832674
m9321 <mahmoud.m9321@yahoo.com>
2015-02-10 13:29:18 +0300

    Test from mac

--- app/src/main/java/feeder/rss/mmm/rssfeeder/MainActivity.java
+++ app/src/main/java/feeder/rss/mmm/rssfeeder/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //teeeeeeeeeeeeeeeeeeeeeeeeesssst
+        //test from mac
     }
 
 
